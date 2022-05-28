e6e6ec3d788d495ccb1bfa706410ccfb0047f128
Kavindu Chamith <kavindu.chamith@pagero.com>
2022-05-28 14:25:17 +0530

    [CAA-9]: added auth middleware for portions

--- src/routers/portion.router.js
+++ src/routers/portion.router.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const portionController = require('../controllers/portion.controller');
-
-router.post('/', portionController.create_portion);
+const { AuthorizationMiddleware } = require("../middlewares/authorization.middleware");
 
 router.get('/', portionController.get_all_portions);
 
-router.patch('/:portionId', portionController.update_portion);
+router.post('/', AuthorizationMiddleware('customer'), portionController.create_portion);
 
-router.delete('/:portionId', portionController.delete_portion);
+router.patch('/:portionId', AuthorizationMiddleware('customer'),portionController.update_portion);
+
+router.delete('/:portionId', AuthorizationMiddleware('customer'),portionController.delete_portion);
 
 
 /**
